e28d4e2178419f4f7e6e1c4dcf63061392935d46
tarassenkoe <73853172+tarassenkoe@users.noreply.github.com>
2023-10-08 16:43:16 -0400

    Update app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+function init() 
+{d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/
+         14-Interactive-Web-Visualizations/02-Homework/samples.json")}
+  
+                 
